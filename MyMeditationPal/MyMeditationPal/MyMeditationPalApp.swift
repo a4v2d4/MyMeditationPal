ea43f122496f267f5e883c6f1db3fdeb74e9cd00
Aaron Van Doren <vandoren96+1@gmail.com>
2026-01-18 13:50:50 -0800

    Add template Xcode project files.

---
+++ MyMeditationPal/MyMeditationPal/MyMeditationPalApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyMeditationPalApp.swift
+//  MyMeditationPal
+//
+//  Created by Aaron Van Doren on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MyMeditationPalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MyMeditationPal/MyMeditationPalTests/MyMeditationPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMeditationPalTests.swift
+//  MyMeditationPalTests
+//
+//  Created by Aaron Van Doren on 1/18/26.
+//
+
+import Testing
+@testable import MyMeditationPal
+
+struct MyMeditationPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
